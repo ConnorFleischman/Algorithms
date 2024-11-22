b5a34f07296e9cd4a188779da8332886cb855e1c
ConnorFleischman <connor.fleischman1@marist.edu>
2024-11-22 13:29:22 -0500

    A3 WIP
    My plan is to get this assignment done before break. In order to leave time for other class work. I hope to have the UndirectedGraph.h made into DirectedGraph.h and implement the necessary requirements.

--- Assignment_4/src/Main.cpp
+++ Assignment_4/src/Main.cpp
@@ -1,1 +1,3 @@
 // Algorithms ~ A.Labouseur, Assignment 4 - Connor Fleischman
+
+using namespace std; // Globally used namespace, removing use of std::
--- Assignment_4/src/Parse.h
+++ Assignment_4/src/Parse.h
@@ -1,1 +1,5 @@
 // Algorithms ~ A.Labouseur, Assignment 4 - Connor Fleischman
+
+#include <fstream>
+
+std::ifstream file("./input/graphs2.txt");
