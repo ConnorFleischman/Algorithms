983b8f7b96b4118fb60004d85f2cb66567f541dc
ConnorFleischman <connor.fleischman1@marist.edu>
2024-11-22 14:34:37 -0500

    Building out DirectedGraph.h
    Adding Insert and Delete for Edges class.  Updated and brought methods from A3. Updated search and still have to build out destructor and deletion functions. And SSSP.

--- Assignment_4/src/graph/DirectedGraph.h
+++ Assignment_4/src/graph/DirectedGraph.h
@@ -2,6 +2,7 @@
 #ifndef H_DIRECTEDGRAPH
 #define H_DIRECTEDGRAPH
 
+#include <iostream>
 #include <vector>
 #include <string>
 
@@ -21,15 +22,15 @@ public:
 class Edge // Defines the Edge class
 {
 public:
-   Vertex *start; // Identifies the starting vertex of the edge
-   Vertex *end;   // Identifies the ending vertex of the edge
-   int weight;    // Identifies the weight of the edge
+   std::string startID; // Identifies the starting vertex ID of the edge
+   std::string endID;   // Identifies the ending vertex ID of the edge
+   int weight;          // Identifies the weight of the edge
 
-   Edge(Vertex *v1, Vertex *v2, int cost) // Edge constructor
+   Edge(std::string v1ID, std::string v2ID, int cost) // Edge constructor
    {
-      this->start = v1;    // Set this edge's start to v1
-      this->end = v2;      // Set this edge's end to v2
-      this->weight = cost; // Set this edge's weight to cost
+      this->startID = v1ID; // Set this edge's start to v1
+      this->endID = v2ID;   // Set this edge's end to v2
+      this->weight = cost;  // Set this edge's weight to cost
    }
 };
 
@@ -41,6 +42,89 @@ private:
 public:
    Graph() {}             // Graph constructor
    ~Graph() { /*TODO:*/ } // Graph destructor
+
+   void insertVertex(std::string &vertexID) // Insert a vertex into the graph by some ID
+   {
+      Vertex *newVertex = new Vertex(vertexID); // create new vertex with some ID
+      vertices.push_back(newVertex);            // add that new vertex to vertices vector
+      std::cout << "Vertex #" << vertexID << " created" << std::endl;
+   }
+
+   void insertEdge(std::string &vertexStartID, std::string &vertexEndID, int edgeWeight) // Insert an edge between two vertex IDs
+   {
+      Vertex *vertexStart = search(vertexStartID); // Search for the starting vertex by its ID
+      Vertex *vertexEnd = search(vertexEndID);     // Search for the ending vertex by its ID
+
+      if (vertexStart && vertexEnd) // If both vertices exist
+      {
+         Edge *newEdge = new Edge(vertexStart->id, vertexEnd->id, edgeWeight); // create a new edge between two vertices's ID's with some weight
+         edges.push_back(newEdge);                                             // add that new edge to the edges vector
+         vertexStart->neighbors.push_back(vertexEnd);                          // Update the starting vertex's neighbors list so that it has the ending vertex as a neighbor
+         std::cout << "Edge created from vertex #" << vertexStartID << " to #" << vertexEndID << " with weight: " << edgeWeight << std::endl;
+      }
+      else // If either vertex does not exist
+      {
+         if (vertexStart == nullptr) // If the start vertex does not exist
+         {
+            std::cout << "Error: Vertex #" << vertexStartID << " does not exist" << std::endl;
+         }
+         if (vertexEnd == nullptr) // If the end vertex does not exist
+         {
+            std::cout << "Error: Vertex #" << vertexEndID << " does not exist" << std::endl;
+         }
+         // Regardless:
+         std::cout << "Cannot insert edge from Vertex: " << vertexStartID << " to Vertex: " << vertexEndID << std::endl;
+      }
+   }
+
+   void deleteVertex(std::string &vertexID) // Deletes a vertex by its ID string
+   {
+      // TODO: Delete Vertex Implementation
+   }
+
+   void deleteEdge(std::string v1ID, std::string v2ID) // Deletes an edge between two vertices's ID's
+   {
+      // TODO: Delete Edge Implementation
+   }
+
+   Vertex *search(std::string &vertexID) // Search for a vertex in the graph by its ID, return the Vertex or null
+   {
+      for (Vertex *vertex : vertices) // For every vertex in the graph
+      {
+         if (vertex->id == vertexID) // If their ID is the ID being searched
+         {
+            return vertex; // Return that vertex
+         }
+      }
+      // If the vertex is not found
+      return nullptr;
+   }
+
+   bool isEmpty() // Returns true/false depending on if the graph is empty
+   {
+      return vertices.empty();
+   }
+
+   void clear() // Empties the graph
+   {
+      for (Vertex *vertex : vertices) // For every vertex in the graph
+      {
+         deleteVertex(vertex->id); // Delete that vertex, allocate memory space
+      }
+      vertices.clear(); // Clear vertices
+
+      for (Edge *edge : edges) // For every edge in the graph
+      {
+         Vertex *startVertex = search(edge->startID);
+         Vertex *endVertex = search(edge->endID);
+         deleteEdge(startVertex->id, endVertex->id); // Delete that edge,  between two vertices's ID's, allocate memory space
+      }
+      edges.clear(); // Clear edges
+
+      std::cout << "Graph emptied" << std::endl;
+   }
+
+   // TODO: Single Source Shortest Path Algorithm Implementation
 };
 
 #endif
