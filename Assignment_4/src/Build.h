4def7faa256f9c16f1b99e414d534b88e5492ba6
ConnorFleischman <connor.fleischman1@marist.edu>
2024-11-22 13:43:27 -0500

    A3 WIP
    Building out Build.h and Parse.h and renamed graph file.

--- Assignment_4/src/Build.h
+++ Assignment_4/src/Build.h
@@ -1,1 +1,34 @@
 // Algorithms ~ A.Labouseur, Assignment 4 - Connor Fleischman
+#ifndef H_BUILD
+#define H_BUILD
+
+#include "Parse.h"
+#include <sstream>
+
+void buildGraph(std::vector<std::string> &graphInstructions, Graph &graph) // Given the instructions for one graph and the graph, build that graph
+{
+   for (std::string &instruction : graphInstructions) // For every instruction in the vector of instructions
+   {
+      std::istringstream inst(instruction);    // Break the instruction into individual words
+      std::string identifier, opcode, operand; // Declare both identifier, opcode, and operand as strings
+      inst >> identifier >> opcode;            // Read the first two words to their strings
+      getline(inst, operand);                  // Read the remaining words to the string
+
+      if (opcode == "vertex") // If the opcode is named vertex
+      {
+         std::istringstream vID(operand); // Break the operand into just the string, no white-space
+         std::string num;                 // Declare num as a string
+         vID >> num;                      // Read the vertex ID from the operand into num
+         graph.insertVertex(num);         // add a vertex with id: num to the graph
+      }
+      if (opcode == "edge") // If the opcode is edge
+      {
+         std::istringstream edge(operand); // Break the operand into individual words
+         std::string v1, bridge, v2;       // Declare the vertices being connected and the connection symbol
+         edge >> v1 >> bridge >> v2;       // Read the first three words to these strings
+         graph.insertEdge(v1, v2);         // add a edge from v1 - v2
+      }
+   }
+}
+
+#endif
--- Assignment_4/src/Parse.h
+++ Assignment_4/src/Parse.h
@@ -1,5 +1,9 @@
 // Algorithms ~ A.Labouseur, Assignment 4 - Connor Fleischman
+#ifndef H_PARSE
+#define H_PARSE
 
 #include <fstream>
 
 std::ifstream file("./input/graphs2.txt");
+
+#endif
